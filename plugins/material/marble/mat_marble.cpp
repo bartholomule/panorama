7d25de4961203c4eb9a51e56fb912d53278ff21c
angel <angel>
1999-01-24 12:07:16 +0000

    Added TMaterialMarble.

---
+++ plugins/material/marble/mat_marble.cpp
@@ -0,0 +1,164 @@
+/*
+*  Copyright (C) 1998 Angel Jimenez Jimenez and Carlos Jimenez Moreno
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful, but
+*  WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <cmath>
+#include "mat_marble.h"
+
+DEFINE_PLUGIN ("MaterialMarble", FX_MATERIAL_CLASS, TMaterialMarble);
+
+
+
+int TMaterialMarble::setAttribute (const string& rktNAME, NAttribute nVALUE, EAttribType eTYPE)
+{
+
+  if ( rktNAME == "base_color" )
+  {
+    if ( eTYPE == FX_COLOR )
+    {
+      setBaseColor (*((TColor*) nVALUE.pvValue));
+    }
+    else
+    {
+      return FX_ATTRIB_WRONG_TYPE;
+    }
+  }
+  else if ( rktNAME == "zoom" )
+  {
+    if ( eTYPE == FX_VECTOR )
+    {
+      tZoom = *((TVector*) nVALUE.pvValue);
+      tZoom.set (1.0 / tZoom.x(), 1.0 / tZoom.y(), 1.0 / tZoom.z());
+    }
+    else
+    {
+      return FX_ATTRIB_WRONG_TYPE;
+    }
+  }
+  else if ( rktNAME == "bump" )
+  {
+    if ( eTYPE == FX_REAL )
+    {
+      tBumpFactor = nVALUE.dValue;
+    }
+    else
+    {
+      return FX_ATTRIB_WRONG_TYPE;
+    }
+  }
+  else if ( rktNAME == "start_freq" )
+  {
+    if ( eTYPE == FX_REAL )
+    {
+      if ( nVALUE.dValue > 0.0 )
+      {
+        tStartFreq = nVALUE.dValue;
+        tEndFreq   = tStartFreq;
+        
+        for (int i = 0; i < (int) tNumOctaves; i++)
+        {
+          tEndFreq *= 2.17;
+        }
+      }
+      else
+      {
+        return FX_ATTRIB_WRONG_VALUE;
+      }
+    }
+    else
+    {
+      return FX_ATTRIB_WRONG_TYPE;
+    }
+  }
+  else if ( rktNAME == "octaves" )
+  {
+    if ( eTYPE == FX_REAL )
+    {
+      if ( nVALUE.dValue >= 1.0 )
+      {
+        tNumOctaves = nVALUE.dValue;
+        tEndFreq   = tStartFreq;
+        
+        for (int i = 0; i < (int) tNumOctaves; i++)
+        {
+          tEndFreq *= 2.17;
+        }
+      }
+      else
+      {
+        return FX_ATTRIB_WRONG_VALUE;
+      }
+    }
+    else
+    {
+      return FX_ATTRIB_WRONG_TYPE;
+    }
+  }
+  else
+  {
+    return TMaterial::setAttribute (rktNAME, nVALUE, eTYPE);
+  }
+
+  return FX_ATTRIB_OK;
+
+}  /* setAttribute() */
+
+
+int TMaterialMarble::getAttribute (const string& rktNAME, NAttribute& rnVALUE)
+{
+
+  if ( rktNAME == "base_color" )
+  {
+    rnVALUE.pvValue = &tBaseColor;
+  }
+  else if ( rktNAME == "zoom" )
+  {
+    // [_ERROR_] It should return the inverse of this vector.
+    rnVALUE.pvValue = &tZoom;
+  }
+  else if ( rktNAME == "bump" )
+  {
+    rnVALUE.dValue = tBumpFactor;
+  }
+  else if ( rktNAME == "start_freq" )
+  {
+    rnVALUE.dValue = tStartFreq;
+  }
+  else if ( rktNAME == "octaves" )
+  {
+    rnVALUE.dValue = tNumOctaves;
+  }
+  else
+  {
+    return TMaterial::getAttribute (rktNAME, rnVALUE);
+  }
+
+  return FX_ATTRIB_OK;
+
+}  /* getAttribute() */
+
+
+void TMaterialMarble::getAttributeList (TAttributeList& rtLIST) const
+{
+
+  TMaterial::getAttributeList (rtLIST);
+
+  rtLIST ["base_color"] = FX_COLOR;
+  rtLIST ["zoom"]       = FX_VECTOR;
+  rtLIST ["bump"]       = FX_REAL;
+
+}  /* getAttributeList() */
---
+++ plugins/material/marble/mat_marble.h
@@ -0,0 +1,124 @@
+/*
+*  Copyright (C) 1998 Angel Jimenez Jimenez and Carlos Jimenez Moreno
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful, but
+*  WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef _MAT_MARBLE__
+#define _MAT_MARBLE__
+
+#include "llapi/perlin_noise.h"
+#include "llapi/material.h"
+#include "hlapi/plugin_manager.h"
+
+class TMaterialMarble : public TMaterial
+{
+
+  protected:
+
+    TColor         tBaseColor;
+    TVector        tZoom;
+    TScalar        tBumpFactor;
+    TScalar        tStartFreq;
+    TScalar        tEndFreq;
+    TScalar        tNumOctaves;
+    TPerlinNoise   tNoise;
+    
+    TScalar evaluate (const TVector& rktPOINT, TVector* ptGRADIENT = NULL) const;
+
+  public:
+
+    static TBaseClass* _create (const TBaseClass* pktPARENT);
+
+    TMaterialMarble (void) :
+      TMaterial(),
+      tZoom (1, 1, 1),
+      tBumpFactor (0),
+      tStartFreq (0.75),
+      tEndFreq (4.0),
+      tNumOctaves (3.0){}
+      
+    TColor color (const TSurfaceData& rktDATA) const;    
+    TVector perturbNormal (const TSurfaceData& rktDATA) const;
+    int setAttribute (const string& rktNAME, NAttribute nVALUE, EAttribType eTYPE);
+    int getAttribute (const string& rktNAME, NAttribute& rnVALUE);
+    void getAttributeList (TAttributeList& rtLIST) const;
+
+    virtual void setBaseColor (const TColor& rktCOLOR)
+    {
+      tBaseColor = rktCOLOR;
+    }
+
+    string className (void) const { return "MaterialMarble"; }
+
+};  /* class TMaterialMarble */
+
+
+inline TScalar TMaterialMarble::evaluate (const TVector& rktPOINT, TVector* ptGRADIENT) const
+{
+  //TScalar   tValue = tNoise.turbulence (rktPOINT, 0.5, 4.0);
+  //TScalar   tValue = tNoise.turbulence (rktPOINT, 0.75, 4.0);
+
+  TScalar   tValue = tNoise.turbulence (rktPOINT, tStartFreq, tEndFreq);
+  
+  if (ptGRADIENT)
+  {
+    TVector   tDx, tDy, tDz;
+    TScalar   tVx, tVy, tVz;
+
+    tDx.set (1.0E-6, 0.0, 0.0);
+    tDy.set (0.0, 1.0E-6, 0.0);
+    tDz.set (0.0, 0.0, 1.0E-6);
+    
+    tVx = tValue - evaluate (rktPOINT + tDx);
+    tVy = tValue - evaluate (rktPOINT + tDy);
+    tVz = tValue - evaluate (rktPOINT + tDz);
+    
+    ptGRADIENT->set (tVx, tVy, tVz);
+    ptGRADIENT->normalize();
+  }
+
+  return ( tValue < 0.0 ) ? 0.0 : ( tValue > 1.0 ) ? 1.0 : tValue;
+}
+
+
+inline TColor TMaterialMarble::color (const TSurfaceData& rktDATA) const  
+{
+  TVector tPoint = rktDATA.localPoint() * tZoom;
+  TScalar tValue = evaluate (tPoint);
+  
+  return lerp (tBaseColor, tColor, tValue);
+}
+
+inline TVector TMaterialMarble::perturbNormal (const TSurfaceData& rktDATA) const
+{
+  TVector   tNewNormal = rktDATA.normal();
+
+  if ( tBumpFactor )
+  {
+    TVector   tGradient;
+    TVector   tPoint      = rktDATA.localPoint() * tZoom;
+
+    evaluate (tPoint, &tGradient);
+
+    tNewNormal = rktDATA.normal() + tGradient * tBumpFactor;
+    tNewNormal.normalize();
+  }
+  
+  return tNewNormal;
+  
+}  /* perturbNormal() */
+
+#endif  /* _MAT_MARBLE__ */
