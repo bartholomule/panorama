6f4d4d0d69075c4aa4413b8cd6bb545a01c874f4
angel <angel>
1999-06-20 18:26:37 +0000

    Fixed copyright notice.

--- llapi/attribs.h
+++ llapi/attribs.h
@@ -1,5 +1,5 @@
 /*
-*  Copyright (C) 1999 Angel Jimenez Jimenez
+*  Copyright (C) 1999 Angel Jimenez Jimenez and Carlos Jimenez Moreno
 *
 *  This program is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
