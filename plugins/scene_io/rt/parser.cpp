b10e5ab3cf1bfd7ad294f5fa2fc0bb100771e8f6
angel <angel>
1999-08-21 12:54:30 +0000

    Updated.

--- plugins/scene_io/rt/parser.cpp
+++ plugins/scene_io/rt/parser.cpp
@@ -1,6 +1,6 @@
 
 /*  A Bison parser, made from parser.y
- by  GNU Bison version 1.25
+ by  GNU Bison version 1.27
   */
 
 #define YYBISON 1  /* Identify Bison output.  */
@@ -12,54 +12,54 @@
 #define yychar rt_char
 #define yydebug rt_debug
 #define yynerrs rt_nerrs
-#define	T_BOOL	258
-#define	T_REAL	259
-#define	T_IDENTIFIER	260
-#define	T_QUOTED_STRING	261
-#define	T_AGGREGATE	262
-#define	T_ATM_OBJECT	263
-#define	T_BLUE	264
-#define	T_BOX	265
-#define	T_BSDF	266
-#define	T_CAMERA	267
-#define	T_CIRCLE	268
-#define	T_CLASS	269
-#define	T_COLOR	270
-#define	T_CONE	271
-#define	T_CYLINDER	272
-#define	T_DEFINE	273
-#define	T_DIFFERENCE	274
-#define	T_EXTENDS	275
-#define	T_FILTER	276
-#define	T_GREEN	277
-#define	T_IMAGE_FILTER	278
-#define	T_INTERSECTION	279
-#define	T_LIGHT	280
-#define	T_MATERIAL	281
-#define	T_MESH	282
-#define	T_OBJECT	283
-#define	T_OBJECT_FILTER	284
-#define	T_OUTPUT	285
-#define	T_PHONG_TRIANGLE	286
-#define	T_PLANE	287
-#define	T_RECTANGLE	288
-#define	T_RED	289
-#define	T_RENDERER	290
-#define	T_ROTATE	291
-#define	T_SCALE	292
-#define	T_SCENE	293
-#define	T_SPHERE	294
-#define	T_TORUS	295
-#define	T_TRANSLATE	296
-#define	T_TRIANGLE	297
-#define	T_TYPE	298
-#define	T_UNION	299
-#define	T_VECTOR	300
-#define	T_VERTEX	301
-#define	T_X	302
-#define	T_Y	303
-#define	T_Z	304
-#define	UNARY_MINUS	305
+#define	T_BOOL	257
+#define	T_REAL	258
+#define	T_IDENTIFIER	259
+#define	T_QUOTED_STRING	260
+#define	T_AGGREGATE	261
+#define	T_ATM_OBJECT	262
+#define	T_BLUE	263
+#define	T_BOX	264
+#define	T_BSDF	265
+#define	T_CAMERA	266
+#define	T_CIRCLE	267
+#define	T_CLASS	268
+#define	T_COLOR	269
+#define	T_CONE	270
+#define	T_CYLINDER	271
+#define	T_DEFINE	272
+#define	T_DIFFERENCE	273
+#define	T_EXTENDS	274
+#define	T_FILTER	275
+#define	T_GREEN	276
+#define	T_IMAGE_FILTER	277
+#define	T_INTERSECTION	278
+#define	T_LIGHT	279
+#define	T_MATERIAL	280
+#define	T_MESH	281
+#define	T_OBJECT	282
+#define	T_OBJECT_FILTER	283
+#define	T_OUTPUT	284
+#define	T_PHONG_TRIANGLE	285
+#define	T_PLANE	286
+#define	T_RECTANGLE	287
+#define	T_RED	288
+#define	T_RENDERER	289
+#define	T_ROTATE	290
+#define	T_SCALE	291
+#define	T_SCENE	292
+#define	T_SPHERE	293
+#define	T_TORUS	294
+#define	T_TRANSLATE	295
+#define	T_TRIANGLE	296
+#define	T_TYPE	297
+#define	T_UNION	298
+#define	T_VECTOR	299
+#define	T_VERTEX	300
+#define	T_X	301
+#define	T_Y	302
+#define	T_Z	303
+#define	UNARY_MINUS	304
 
 #line 19 "parser.y"
 
@@ -154,7 +154,7 @@ typedef union {
 #define	YYFLAG		-32768
 #define	YYNTBASE	63
 
-#define YYTRANSLATE(x) ((unsigned)(x) <= 305 ? yytranslate[x] : 200)
+#define YYTRANSLATE(x) ((unsigned)(x) <= 304 ? yytranslate[x] : 200)
 
 static const char yytranslate[] = {     0,
      2,     2,     2,     2,     2,     2,     2,     2,     2,     2,
@@ -182,12 +182,12 @@ static const char yytranslate[] = {     0,
      2,     2,     2,     2,     2,     2,     2,     2,     2,     2,
      2,     2,     2,     2,     2,     2,     2,     2,     2,     2,
      2,     2,     2,     2,     2,     2,     2,     2,     2,     2,
-     2,     2,     2,     2,     2,     1,     2,     3,     4,     5,
-     6,     7,     8,     9,    10,    11,    12,    13,    14,    15,
-    16,    17,    18,    19,    20,    21,    22,    23,    24,    25,
-    26,    27,    28,    29,    30,    31,    32,    33,    34,    35,
-    36,    37,    38,    39,    40,    41,    42,    43,    44,    45,
-    46,    47,    48,    49,    54
+     2,     2,     2,     2,     2,     1,     3,     4,     5,     6,
+     7,     8,     9,    10,    11,    12,    13,    14,    15,    16,
+    17,    18,    19,    20,    21,    22,    23,    24,    25,    26,
+    27,    28,    29,    30,    31,    32,    33,    34,    35,    36,
+    37,    38,    39,    40,    41,    42,    43,    44,    45,    46,
+    47,    48,    49,    54
 };
 
 #if YYDEBUG != 0
@@ -883,6 +883,7 @@ static const short yycheck[] = {     2,
 };
 /* -*-C-*-  Note some compilers choke on comments on `#line' lines.  */
 #line 3 "/usr/lib/bison.simple"
+/* This file comes from bison-1.27.  */
 
 /* Skeleton output parser for bison,
    Copyright (C) 1984, 1989, 1990 Free Software Foundation, Inc.
@@ -899,46 +900,66 @@ static const short yycheck[] = {     2,
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 /* As a special exception, when this file is copied by Bison into a
    Bison output file, you may use that output file without restriction.
    This special exception was added by the Free Software Foundation
    in version 1.24 of Bison.  */
 
-#ifndef alloca
-#ifdef __GNUC__
-#define alloca __builtin_alloca
-#else /* not GNU C.  */
-#if (!defined (__STDC__) && defined (sparc)) || defined (__sparc__) || defined (__sparc) || defined (__sgi)
-#include <alloca.h>
-#else /* not sparc */
-#if defined (MSDOS) && !defined (__TURBOC__)
-#include <malloc.h>
-#else /* not MSDOS, or __TURBOC__ */
-#if defined(_AIX)
-#include <malloc.h>
- #pragma alloca
-#else /* not MSDOS, __TURBOC__, or _AIX */
-#ifdef __hpux
-#ifdef __cplusplus
-extern "C" {
-void *alloca (unsigned int);
-};
-#else /* not __cplusplus */
-void *alloca ();
-#endif /* not __cplusplus */
-#endif /* __hpux */
-#endif /* not _AIX */
-#endif /* not MSDOS, or __TURBOC__ */
-#endif /* not sparc.  */
-#endif /* not GNU C.  */
-#endif /* alloca not defined.  */
-
 /* This is the parser code that is written into each bison parser
   when the %semantic_parser declaration is not specified in the grammar.
   It was written by Richard Stallman by simplifying the hairy parser
   used when %semantic_parser is specified.  */
+
+#ifndef YYSTACK_USE_ALLOCA
+#ifdef alloca
+#define YYSTACK_USE_ALLOCA
+#else /* alloca not defined */
+#ifdef __GNUC__
+#define YYSTACK_USE_ALLOCA
+#define alloca __builtin_alloca
+#else /* not GNU C.  */
+#if (!defined (__STDC__) && defined (sparc)) || defined (__sparc__) || defined (__sparc) || defined (__sgi) || (defined (__sun) && defined (__i386))
+#define YYSTACK_USE_ALLOCA
+#include <alloca.h>
+#else /* not sparc */
+/* We think this test detects Watcom and Microsoft C.  */
+/* This used to test MSDOS, but that is a bad idea
+   since that symbol is in the user namespace.  */
+#if (defined (_MSDOS) || defined (_MSDOS_)) && !defined (__TURBOC__)
+#if 0 /* No need for malloc.h, which pollutes the namespace;
+	 instead, just don't use alloca.  */
+#include <malloc.h>
+#endif
+#else /* not MSDOS, or __TURBOC__ */
+#if defined(_AIX)
+/* I don't know what this was needed for, but it pollutes the namespace.
+   So I turned it off.   rms, 2 May 1997.  */
+/* #include <malloc.h>  */
+ #pragma alloca
+#define YYSTACK_USE_ALLOCA
+#else /* not MSDOS, or __TURBOC__, or _AIX */
+#if 0
+#ifdef __hpux /* haible@ilog.fr says this works for HPUX 9.05 and up,
+		 and on HPUX 10.  Eventually we can turn this on.  */
+#define YYSTACK_USE_ALLOCA
+#define alloca __builtin_alloca
+#endif /* __hpux */
+#endif
+#endif /* not _AIX */
+#endif /* not MSDOS, or __TURBOC__ */
+#endif /* not sparc */
+#endif /* not GNU C */
+#endif /* alloca not defined */
+#endif /* YYSTACK_USE_ALLOCA not defined */
+
+#ifdef YYSTACK_USE_ALLOCA
+#define YYSTACK_ALLOC alloca
+#else
+#define YYSTACK_ALLOC malloc
+#endif
 
 /* Note: there must be only one dollar sign in this file.
    It is replaced by the list of actions, each action
@@ -948,8 +969,8 @@ void *alloca ();
 #define yyclearin	(yychar = YYEMPTY)
 #define YYEMPTY		-2
 #define YYEOF		0
-#define YYACCEPT	return(0)
-#define YYABORT 	return(1)
+#define YYACCEPT	goto yyacceptlab
+#define YYABORT 	goto yyabortlab
 #define YYERROR		goto yyerrlab1
 /* Like YYERROR except do call yyerror.
    This remains here temporarily to ease the
@@ -1030,12 +1051,12 @@ int yydebug;			/*  nonzero means print parse trace	*/
 #ifndef YYMAXDEPTH
 #define YYMAXDEPTH 10000
 #endif
-
-/* Prevent warning if -Wstrict-prototypes.  */
-#ifdef __GNUC__
-int yyparse (void);
-#endif
 
+/* Define __yy_memcpy.  Note that the size argument
+   should be passed with type unsigned int, because that is what the non-GCC
+   definitions require.  With GCC, __builtin_memcpy takes an arg
+   of type size_t, but it can handle unsigned int.  */
+
 #if __GNUC__ > 1		/* GNU C and GNU C++ define this.  */
 #define __yy_memcpy(TO,FROM,COUNT)	__builtin_memcpy(TO,FROM,COUNT)
 #else				/* not GNU C or C++ */
@@ -1047,7 +1068,7 @@ static void
 __yy_memcpy (to, from, count)
      char *to;
      char *from;
-     int count;
+     unsigned int count;
 {
   register char *f = from;
   register char *t = to;
@@ -1062,10 +1083,10 @@ __yy_memcpy (to, from, count)
 /* This is the most reliable way to avoid incompatibilities
    in available built-in functions on various systems.  */
 static void
-__yy_memcpy (char *to, char *from, int count)
+__yy_memcpy (char *to, char *from, unsigned int count)
 {
-  register char *f = from;
   register char *t = to;
+  register char *f = from;
   register int i = count;
 
   while (i-- > 0)
@@ -1075,7 +1096,7 @@ __yy_memcpy (char *to, char *from, int count)
 #endif
 #endif
 
-#line 196 "/usr/lib/bison.simple"
+#line 216 "/usr/lib/bison.simple"
 
 /* The user can define YYPARSE_PARAM as the name of an argument to be passed
    into yyparse.  The argument should have type void *.
@@ -1095,6 +1116,15 @@ __yy_memcpy (char *to, char *from, int count)
 #define YYPARSE_PARAM_ARG
 #define YYPARSE_PARAM_DECL
 #endif /* not YYPARSE_PARAM */
+
+/* Prevent warning if -Wstrict-prototypes.  */
+#ifdef __GNUC__
+#ifdef YYPARSE_PARAM
+int yyparse (void *);
+#else
+int yyparse (void);
+#endif
+#endif
 
 int
 yyparse(YYPARSE_PARAM_ARG)
@@ -1124,6 +1154,7 @@ yyparse(YYPARSE_PARAM_ARG)
 #endif
 
   int yystacksize = YYINITDEPTH;
+  int yyfree_stacks = 0;
 
 #ifdef YYPURE
   int yychar;
@@ -1208,18 +1239,32 @@ yynewstate:
       if (yystacksize >= YYMAXDEPTH)
 	{
 	  yyerror("parser stack overflow");
+	  if (yyfree_stacks)
+	    {
+	      free (yyss);
+	      free (yyvs);
+#ifdef YYLSP_NEEDED
+	      free (yyls);
+#endif
+	    }
 	  return 2;
 	}
       yystacksize *= 2;
       if (yystacksize > YYMAXDEPTH)
 	yystacksize = YYMAXDEPTH;
-      yyss = (short *) alloca (yystacksize * sizeof (*yyssp));
-      __yy_memcpy ((char *)yyss, (char *)yyss1, size * sizeof (*yyssp));
-      yyvs = (YYSTYPE *) alloca (yystacksize * sizeof (*yyvsp));
-      __yy_memcpy ((char *)yyvs, (char *)yyvs1, size * sizeof (*yyvsp));
+#ifndef YYSTACK_USE_ALLOCA
+      yyfree_stacks = 1;
+#endif
+      yyss = (short *) YYSTACK_ALLOC (yystacksize * sizeof (*yyssp));
+      __yy_memcpy ((char *)yyss, (char *)yyss1,
+		   size * (unsigned int) sizeof (*yyssp));
+      yyvs = (YYSTYPE *) YYSTACK_ALLOC (yystacksize * sizeof (*yyvsp));
+      __yy_memcpy ((char *)yyvs, (char *)yyvs1,
+		   size * (unsigned int) sizeof (*yyvsp));
 #ifdef YYLSP_NEEDED
-      yyls = (YYLTYPE *) alloca (yystacksize * sizeof (*yylsp));
-      __yy_memcpy ((char *)yyls, (char *)yyls1, size * sizeof (*yylsp));
+      yyls = (YYLTYPE *) YYSTACK_ALLOC (yystacksize * sizeof (*yylsp));
+      __yy_memcpy ((char *)yyls, (char *)yyls1,
+		   size * (unsigned int) sizeof (*yylsp));
 #endif
 #endif /* no yyoverflow */
 
@@ -2830,7 +2875,7 @@ case 264:
     break;}
 }
    /* the action file gets copied in in place of this dollarsign */
-#line 498 "/usr/lib/bison.simple"
+#line 542 "/usr/lib/bison.simple"
 
   yyvsp -= yylen;
   yyssp -= yylen;
@@ -3025,6 +3070,30 @@ yyerrhandle:
 
   yystate = yyn;
   goto yynewstate;
+
+ yyacceptlab:
+  /* YYACCEPT comes here.  */
+  if (yyfree_stacks)
+    {
+      free (yyss);
+      free (yyvs);
+#ifdef YYLSP_NEEDED
+      free (yyls);
+#endif
+    }
+  return 0;
+
+ yyabortlab:
+  /* YYABORT comes here.  */
+  if (yyfree_stacks)
+    {
+      free (yyss);
+      free (yyvs);
+#ifdef YYLSP_NEEDED
+      free (yyls);
+#endif
+    }
+  return 1;
 }
 #line 1418 "parser.y"
 
@@ -3192,12 +3261,17 @@ void SetParameter (const string& rktATTRIB, EAttribType eTYPE)
 
   if ( iResult == FX_ATTRIB_WRONG_PARAM )
   {
-    yyerror ("object does not have such parameter");
+    yyerror ("object does not have such attribute");
     exit (1);
   }
   else if ( iResult == FX_ATTRIB_WRONG_TYPE )
   {
-    yyerror ("incorrect type in parameter value");
+    yyerror ("incorrect type in attribute value");
+    exit (1);
+  }
+  else if ( iResult == FX_ATTRIB_WRONG_VALUE )
+  {
+    yyerror ("value not accepted for this attribute");
     exit (1);
   }
   else if ( iResult == FX_ATTRIB_USER_ERROR )
